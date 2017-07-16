dbdea8b6e54096a94e6af37d24347e2c47100a4f
arusanov <alexander.rusanov@gmail.com>
2017-07-16 10:19:39 +0300

    refomated code

--- test/less-test.js
+++ test/less-test.js
@@ -1,9 +1,9 @@
 /*jshint latedef: nofunc */
 
-module.exports = function () {
-  const path = require('path'),
-    fs = require('fs'),
-    copyBom = require('./copy-bom')()
+module.exports = () => {
+  const path = require('path')
+  const fs = require('fs')
+  const copyBom = require('./copy-bom')()
   let doBomTest = false
 
   const less = require('../lib/less-node')
@@ -20,23 +20,23 @@ module.exports = function () {
   const bomFolder = 'test/less-bom'
 
   less.logger.addListener({
-    info: function (msg) {
+    info(msg) {
       if (isVerbose) {
-        process.stdout.write(msg + '\n')
+        process.stdout.write(`${msg}\n`)
       }
     },
-    warn: function (msg) {
-      process.stdout.write(msg + '\n')
+    warn(msg) {
+      process.stdout.write(`${msg}\n`)
     },
-    error: function (msg) {
-      process.stdout.write(msg + '\n')
-    }
+    error(msg) {
+      process.stdout.write(`${msg}\n`)
+    },
   })
 
   const queueList = []
   let queueRunning = false
 
-  function queue (func) {
+  function queue(func) {
     if (queueRunning) {
       //console.log("adding to queue");
       queueList.push(func)
@@ -47,7 +47,7 @@ module.exports = function () {
     }
   }
 
-  function release () {
+  function release() {
     if (queueList.length) {
       //console.log("running next in queue");
       const func = queueList.shift()
@@ -58,48 +58,67 @@ module.exports = function () {
     }
   }
 
-  let totalTests = 0,
-    failedTests = 0,
-    passedTests = 0
+  let totalTests = 0
+  let failedTests = 0
+  let passedTests = 0
 
   less.functions.functionRegistry.addMultiple({
-    add: function (a, b) {
-      return new (less.tree.Dimension)(a.value + b.value)
+    add(a, b) {
+      return new less.tree.Dimension(a.value + b.value)
     },
-    increment: function (a) {
-      return new (less.tree.Dimension)(a.value + 1)
+    increment(a) {
+      return new less.tree.Dimension(a.value + 1)
     },
-    _color: function (str) {
-      if (str.value === 'evil red') { return new (less.tree.Color)('600') }
-    }
+    _color(str) {
+      if (str.value === 'evil red') {
+        return new less.tree.Color('600')
+      }
+    },
   })
 
-  function testSourcemap (name, err, compiledLess, doReplacements, sourcemap, baseFolder) {
-    fs.readFile(path.join('test/', name) + '.json', 'utf8', function (e, expectedSourcemap) {
-      process.stdout.write('- ' + path.join(baseFolder, name) + ': ')
-      if (sourcemap === expectedSourcemap) {
-        ok('OK')
-      } else if (err) {
-        fail('ERROR: ' + (err && err.message))
-        if (isVerbose) {
-          process.stdout.write('\n')
-          process.stdout.write(err.stack + '\n')
+  function testSourcemap(
+    name,
+    err,
+    compiledLess,
+    doReplacements,
+    sourcemap,
+    baseFolder
+  ) {
+    fs.readFile(
+      `${path.join('test/', name)}.json`,
+      'utf8',
+      (e, expectedSourcemap) => {
+        process.stdout.write(`- ${path.join(baseFolder, name)}: `)
+        if (sourcemap === expectedSourcemap) {
+          ok('OK')
+        } else if (err) {
+          fail(`ERROR: ${err && err.message}`)
+          if (isVerbose) {
+            process.stdout.write('\n')
+            process.stdout.write(`${err.stack}\n`)
+          }
+        } else {
+          difference('FAIL', expectedSourcemap, sourcemap)
         }
-      } else {
-        difference('FAIL', expectedSourcemap, sourcemap)
       }
-    })
+    )
   }
 
-  function testEmptySourcemap (name, err, compiledLess, doReplacements, sourcemap, baseFolder) {
-    process.stdout.write('- ' + path.join(baseFolder, name) + ': ')
+  function testEmptySourcemap(
+    name,
+    err,
+    compiledLess,
+    doReplacements,
+    sourcemap,
+    baseFolder
+  ) {
+    process.stdout.write(`- ${path.join(baseFolder, name)}: `)
     if (err) {
-      fail('ERROR: ' + (err && err.message))
+      fail(`ERROR: ${err && err.message}`)
     } else {
       const expectedSourcemap = undefined
       if (compiledLess !== '') {
         difference('\nCompiledLess must be empty', '', compiledLess)
-
       } else if (sourcemap !== expectedSourcemap) {
         fail('Sourcemap must be undefined')
       } else {
@@ -108,97 +127,160 @@ module.exports = function () {
     }
   }
 
-  function testErrors (name, err, compiledLess, doReplacements, sourcemap, baseFolder) {
-    fs.readFile(path.join(baseFolder, name) + '.txt', 'utf8', function (e, expectedErr) {
-      process.stdout.write('- ' + path.join(baseFolder, name) + ': ')
-      expectedErr = doReplacements(expectedErr, baseFolder)
-      if (!err) {
-        if (compiledLess) {
-          fail('No Error', 'red')
+  function testErrors(
+    name,
+    err,
+    compiledLess,
+    doReplacements,
+    sourcemap,
+    baseFolder
+  ) {
+    fs.readFile(
+      `${path.join(baseFolder, name)}.txt`,
+      'utf8',
+      (e, expectedErr) => {
+        process.stdout.write(`- ${path.join(baseFolder, name)}: `)
+        expectedErr = doReplacements(expectedErr, baseFolder)
+        if (!err) {
+          if (compiledLess) {
+            fail('No Error', 'red')
+          } else {
+            fail('No Error, No Output')
+          }
         } else {
-          fail('No Error, No Output')
-        }
-      } else {
-        const errMessage = less.formatError(err)
-        if (errMessage === expectedErr) {
-          ok('OK')
-        } else {
-          difference('FAIL', expectedErr, errMessage)
+          const errMessage = less.formatError(err)
+          if (errMessage === expectedErr) {
+            ok('OK')
+          } else {
+            difference('FAIL', expectedErr, errMessage)
+          }
         }
       }
-    })
+    )
   }
 
-  function globalReplacements (input, directory) {
-    const p = path.join(process.cwd(), directory),
-      pathimport = path.join(process.cwd(), directory + 'import/'),
-      pathesc = p.replace(/[.:/\\]/g, function (a) { return '\\' + (a == '\\' ? '\/' : a) }),
-      pathimportesc = pathimport.replace(/[.:/\\]/g, function (a) { return '\\' + (a == '\\' ? '\/' : a) })
+  function globalReplacements(input, directory) {
+    const p = path.join(process.cwd(), directory)
+    const pathimport = path.join(process.cwd(), `${directory}import/`)
+    const pathesc = p.replace(/[.:/\\]/g, a => `\\${a == '\\' ? '/' : a}`)
+    const pathimportesc = pathimport.replace(
+      /[.:/\\]/g,
+      a => `\\${a == '\\' ? '/' : a}`
+    )
 
-    return input.replace(/\{path\}/g, p)
+    return input
+      .replace(/\{path\}/g, p)
       .replace(/\{pathesc\}/g, pathesc)
       .replace(/\{pathimport\}/g, pathimport)
       .replace(/\{pathimportesc\}/g, pathimportesc)
       .replace(/\r\n/g, '\n')
   }
 
-  function checkGlobalLeaks () {
-    return Object.keys(global).filter(function (v) {
-      return globals.indexOf(v) < 0
-    })
+  function checkGlobalLeaks() {
+    return Object.keys(global).filter(v => globals.indexOf(v) < 0)
   }
 
-  function testSyncronous (options, filenameNoExtension) {
-    if (oneTestOnly && ('Test Sync ' + filenameNoExtension) !== oneTestOnly) {
+  function testSyncronous(options, filenameNoExtension) {
+    if (oneTestOnly && `Test Sync ${filenameNoExtension}` !== oneTestOnly) {
       return
     }
     totalTests++
-    queue(function () {
+    queue(() => {
       let isSync = true
-      toCSS(options, path.join(normalFolder, filenameNoExtension + '.less'), function (err, result) {
-        process.stdout.write('- Test Sync ' + filenameNoExtension + ': ')
+      toCSS(
+        options,
+        path.join(normalFolder, `${filenameNoExtension}.less`),
+        (err, result) => {
+          process.stdout.write(`- Test Sync ${filenameNoExtension}: `)
 
-        if (isSync) {
-          ok('OK')
-        } else {
-          fail('Not Sync')
+          if (isSync) {
+            ok('OK')
+          } else {
+            fail('Not Sync')
+          }
+          release()
         }
-        release()
-      })
+      )
       isSync = false
     })
   }
 
-  function prepBomTest () {
+  function prepBomTest() {
     copyBom.copyFolderWithBom(normalFolder, bomFolder)
     doBomTest = true
   }
 
-  function runTestSet (options, foldername, verifyFunction, nameModifier, doReplacements, getFilename) {
+  function runTestSet(
+    options,
+    foldername,
+    verifyFunction,
+    nameModifier,
+    doReplacements,
+    getFilename
+  ) {
     const options2 = options ? JSON.parse(JSON.stringify(options)) : {}
-    runTestSetInternal(normalFolder, options, foldername, verifyFunction, nameModifier, doReplacements, getFilename)
+    runTestSetInternal(
+      normalFolder,
+      options,
+      foldername,
+      verifyFunction,
+      nameModifier,
+      doReplacements,
+      getFilename
+    )
     if (doBomTest) {
-      runTestSetInternal(bomFolder, options2, foldername, verifyFunction, nameModifier, doReplacements, getFilename)
+      runTestSetInternal(
+        bomFolder,
+        options2,
+        foldername,
+        verifyFunction,
+        nameModifier,
+        doReplacements,
+        getFilename
+      )
     }
   }
 
-  function runTestSetNormalOnly (options, foldername, verifyFunction, nameModifier, doReplacements, getFilename) {
-    runTestSetInternal(normalFolder, options, foldername, verifyFunction, nameModifier, doReplacements, getFilename)
+  function runTestSetNormalOnly(
+    options,
+    foldername,
+    verifyFunction,
+    nameModifier,
+    doReplacements,
+    getFilename
+  ) {
+    runTestSetInternal(
+      normalFolder,
+      options,
+      foldername,
+      verifyFunction,
+      nameModifier,
+      doReplacements,
+      getFilename
+    )
   }
 
-  function runTestSetInternal (baseFolder, options, foldername, verifyFunction, nameModifier, doReplacements, getFilename) {
-    foldername = foldername || ''
-
+  function runTestSetInternal(
+    baseFolder,
+    options,
+    foldername = '',
+    verifyFunction,
+    nameModifier,
+    doReplacements,
+    getFilename
+  ) {
     if (!doReplacements) {
       doReplacements = globalReplacements
     }
 
-    function getBasename (file) {
+    function getBasename(file) {
       return foldername + path.basename(file, '.less')
     }
 
-    fs.readdirSync(path.join(baseFolder, foldername)).forEach(function (file) {
-      if (!/\.less/.test(file)) { return }
+    fs.readdirSync(path.join(baseFolder, foldername)).forEach(file => {
+      if (!/\.less/.test(file)) {
+        return
+      }
 
       const name = getBasename(file)
 
@@ -209,124 +291,161 @@ module.exports = function () {
       totalTests++
 
       if (options.sourceMap && !options.sourceMap.sourceMapFileInline) {
-        options.sourceMapOutputFilename = name + '.css'
+        options.sourceMapOutputFilename = `${name}.css`
         options.sourceMapBasepath = path.join(process.cwd(), baseFolder)
         options.sourceMapRootpath = 'testweb/'
         // TODO separate options?
         options.sourceMap = options
       }
 
-      options.getVars = function (file) {
-        return JSON.parse(fs.readFileSync(getFilename(getBasename(file), 'vars', baseFolder), 'utf8'))
-      }
+      options.getVars = file =>
+        JSON.parse(
+          fs.readFileSync(
+            getFilename(getBasename(file), 'vars', baseFolder),
+            'utf8'
+          )
+        )
 
       let doubleCallCheck = false
-      queue(function () {
-        toCSS(options, path.join(baseFolder, foldername + file), function (err, result) {
-          if (doubleCallCheck) {
-            totalTests++
-            fail('less is calling back twice')
-            process.stdout.write(doubleCallCheck + '\n')
-            process.stdout.write((new Error()).stack + '\n')
-            return
-          }
-          doubleCallCheck = (new Error()).stack
+      queue(() => {
+        toCSS(
+          options,
+          path.join(baseFolder, foldername + file),
+          (err, result) => {
+            if (doubleCallCheck) {
+              totalTests++
+              fail('less is calling back twice')
+              process.stdout.write(`${doubleCallCheck}\n`)
+              process.stdout.write(`${new Error().stack}\n`)
+              return
+            }
+            doubleCallCheck = new Error().stack
 
-          if (verifyFunction) {
-            const verificationResult = verifyFunction(name, err, result && result.css, doReplacements, result && result.map, baseFolder)
-            release()
-            return verificationResult
-          }
-          if (err) {
-            fail('ERROR: ' + (err && err.message))
-            if (isVerbose) {
-              process.stdout.write('\n')
-              if (err.stack) {
-                process.stdout.write(err.stack + '\n')
-              } else {
-                //this sometimes happen - show the whole error object
-                console.log(err)
+            if (verifyFunction) {
+              const verificationResult = verifyFunction(
+                name,
+                err,
+                result && result.css,
+                doReplacements,
+                result && result.map,
+                baseFolder
+              )
+              release()
+              return verificationResult
+            }
+            if (err) {
+              fail(`ERROR: ${err && err.message}`)
+              if (isVerbose) {
+                process.stdout.write('\n')
+                if (err.stack) {
+                  process.stdout.write(`${err.stack}\n`)
+                } else {
+                  //this sometimes happen - show the whole error object
+                  console.log(err)
+                }
               }
+              release()
+              return
             }
-            release()
-            return
-          }
-          let css_name = name
-          if (nameModifier) { css_name = nameModifier(name) }
-          fs.readFile(path.join('test/css', css_name) + '.css', 'utf8', function (e, css) {
-            process.stdout.write('- ' + path.join(baseFolder, css_name) + ': ')
+            let css_name = name
+            if (nameModifier) {
+              css_name = nameModifier(name)
+            }
+            fs.readFile(
+              `${path.join('test/css', css_name)}.css`,
+              'utf8',
+              (e, css) => {
+                process.stdout.write(`- ${path.join(baseFolder, css_name)}: `)
 
-            css = css && doReplacements(css, path.join(baseFolder, foldername))
-            if (result.css.trim() === css.trim()) { ok('OK') }
-            else {
-              difference('FAIL', css.trim(), result.css.trim())
-            }
-            release()
-          })
-        })
+                css =
+                  css && doReplacements(css, path.join(baseFolder, foldername))
+                if (result.css.trim() === css.trim()) {
+                  ok('OK')
+                } else {
+                  difference('FAIL', css.trim(), result.css.trim())
+                }
+                release()
+              }
+            )
+          }
+        )
       })
     })
   }
 
-  function diff (left, right) {
-    require('diff').diffLines(left, right).forEach(function (item) {
+  function diff(left, right) {
+    require('diff').diffLines(left, right).forEach(item => {
       if (item.added || item.removed) {
-        const text = item.value && item.value.replace('\n', String.fromCharCode(182) + '\n').replace('\ufeff', '[[BOM]]')
+        const text =
+          item.value &&
+          item.value
+            .replace('\n', `${String.fromCharCode(182)}\n`)
+            .replace('\ufeff', '[[BOM]]')
         process.stdout.write(stylize(text, item.added ? 'green' : 'red'))
       } else {
-        process.stdout.write(item.value && item.value.replace('\ufeff', '[[BOM]]'))
+        process.stdout.write(
+          item.value && item.value.replace('\ufeff', '[[BOM]]')
+        )
       }
     })
     process.stdout.write('\n')
   }
 
-  function fail (msg) {
-    process.stdout.write(stylize(msg, 'red') + '\n')
+  function fail(msg) {
+    process.stdout.write(`${stylize(msg, 'red')}\n`)
     failedTests++
     endTest()
   }
 
-  function difference (msg, left, right) {
-    process.stdout.write(stylize(msg, 'yellow') + '\n')
+  function difference(msg, left, right) {
+    process.stdout.write(`${stylize(msg, 'yellow')}\n`)
     failedTests++
 
     diff(left, right)
     endTest()
   }
 
-  function ok (msg) {
-    process.stdout.write(stylize(msg, 'green') + '\n')
+  function ok(msg) {
+    process.stdout.write(`${stylize(msg, 'green')}\n`)
     passedTests++
     endTest()
   }
 
-  function finished () {
+  function finished() {
     isFinished = true
     endTest()
   }
 
-  function endTest () {
-    if (isFinished && ((failedTests + passedTests) >= totalTests)) {
+  function endTest() {
+    if (isFinished && failedTests + passedTests >= totalTests) {
       const leaked = checkGlobalLeaks()
 
       process.stdout.write('\n')
       if (failedTests > 0) {
-        process.stdout.write(failedTests + stylize(' Failed', 'red') + ', ' + passedTests + ' passed\n')
+        process.stdout.write(
+          `${failedTests + stylize(' Failed', 'red')}, ${passedTests} passed\n`
+        )
       } else {
-        process.stdout.write(stylize('All Passed ', 'green') + passedTests + ' run\n')
+        process.stdout.write(
+          `${stylize('All Passed ', 'green') + passedTests} run\n`
+        )
       }
       if (leaked.length > 0) {
         process.stdout.write('\n')
-        process.stdout.write(stylize('Global leak detected: ', 'red') + leaked.join(', ') + '\n')
+        process.stdout.write(
+          `${stylize('Global leak detected: ', 'red') + leaked.join(', ')}\n`
+        )
       }
 
       if (leaked.length || failedTests) {
-        process.on('exit', function () { process.reallyExit(1) })
+        process.on('exit', () => {
+          process.reallyExit(1)
+        })
       }
     }
   }
 
-  function contains (fullArray, obj) {
+  function contains(fullArray, obj) {
     for (let i = 0; i < fullArray.length; i++) {
       if (fullArray[i] === obj) {
         return true
@@ -335,9 +454,9 @@ module.exports = function () {
     return false
   }
 
-  function toCSS (options, path, callback) {
-    options = options || {}
-    const str = fs.readFileSync(path, 'utf8'), addPath = require('path').dirname(path)
+  function toCSS(options = {}, path, callback) {
+    const str = fs.readFileSync(path, 'utf8')
+    const addPath = require('path').dirname(path)
     if (typeof options.paths !== 'string') {
       options.paths = options.paths || []
       if (!contains(options.paths, addPath)) {
@@ -353,13 +472,16 @@ module.exports = function () {
       options.modifyVars = options.getVars(path)
     }
     if (options.plugin) {
-      const Plugin = require(require('path').resolve(process.cwd(), options.plugin))
+      const Plugin = require(require('path').resolve(
+        process.cwd(),
+        options.plugin
+      ))
       options.plugins = [Plugin]
     }
     less.render(str, options, callback)
   }
 
-  function testNoOptions () {
+  function testNoOptions() {
     if (oneTestOnly && 'Integration' !== oneTestOnly) {
       return
     }
@@ -375,14 +497,14 @@ module.exports = function () {
   }
 
   return {
-    runTestSet: runTestSet,
-    runTestSetNormalOnly: runTestSetNormalOnly,
-    testSyncronous: testSyncronous,
-    testErrors: testErrors,
-    testSourcemap: testSourcemap,
-    testEmptySourcemap: testEmptySourcemap,
-    testNoOptions: testNoOptions,
-    prepBomTest: prepBomTest,
-    finished: finished
+    runTestSet,
+    runTestSetNormalOnly,
+    testSyncronous,
+    testErrors,
+    testSourcemap,
+    testEmptySourcemap,
+    testNoOptions,
+    prepBomTest,
+    finished,
   }
 }
--- test/plugins/visitor/index.js
+++ test/plugins/visitor/index.js
@@ -1,24 +1,23 @@
-(function(exports) {
-    var RemoveProperty = function(less) {
-        this._visitor = new less.visitors.Visitor(this);
-    };
+;(exports => {
+  const RemoveProperty = function(less) {
+    this._visitor = new less.visitors.Visitor(this)
+  }
 
-    RemoveProperty.prototype = {
-        isReplacing: true,
-        run: function (root) {
-            return this._visitor.visit(root);
-        },
-        visitRule: function (ruleNode, visitArgs) {
-            if (ruleNode.name != '-some-aribitrary-property') {
-                return ruleNode;
-            } else {
-                return [];
-            }
-        }
-    };
+  RemoveProperty.prototype = {
+    isReplacing: true,
+    run(root) {
+      return this._visitor.visit(root)
+    },
+    visitRule(ruleNode, visitArgs) {
+      if (ruleNode.name != '-some-aribitrary-property') {
+        return ruleNode
+      } else {
+        return []
+      }
+    },
+  }
 
-    exports.install = function(less, pluginManager) {
-        pluginManager.addVisitor( new RemoveProperty(less));
-    };
-
-})(typeof exports === 'undefined' ? this['VisitorPlugin'] = {} : exports);
+  exports.install = (less, pluginManager) => {
+    pluginManager.addVisitor(new RemoveProperty(less))
+  }
+})(typeof exports === 'undefined' ? (this['VisitorPlugin'] = {}) : exports)
